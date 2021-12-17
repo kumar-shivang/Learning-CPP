fcde113fdac57f46449509f1cc9830af095b8b0f
xi-wang-ku <21f2001304@student.onlinedegree.iitm.ac.in>
2021-12-17 17:13:23 +0530

    first commit

---
+++ out-in.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int num1, num2, sum;
+    cout << "Enter the first number = ";
+    cin >> num1;
+    cout << "Enter the first number = ";
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "sum = " << sum << "\n";
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    float b = 20.22;
+    char c = 'A';
+    cout<<"value of a is = "<< a<<"\nValue of b is = "<<b;
+    cout<<"\nvalue of c is = "<<c;
+    return 0;
+}
+ 
